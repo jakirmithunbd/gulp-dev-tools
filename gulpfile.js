cbaeacccac800c9fbf0ec8ed1e11189f82f5b0db
jakirmithunbd@gmail <jakirmithunbd@gmail.com>
2020-12-15 01:03:54 +0600

    left browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,53 @@
-var gulp = require('gulp'),
-    uglify = require('gulp-uglify');
-concat = require('gulp-concat'),
-    argv = require('yargs').argv,
-    cssminify = require('gulp-minify-css'),
-    cleanCSS = require('gulp-clean-css'),
-    imagemin = require('gulp-imagemin'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps');
+const gulp = require( 'gulp' );
+const sass = require( 'gulp-sass' );
+const uglify = require( 'gulp-uglify' );
+const imagemin = require( 'gulp-imagemin' );
+const concat = require( 'gulp-concat' );
 var browserSync = require('browser-sync').create();
+const { dest } = require('gulp');
 
-//Watch task
-gulp.task('watch:sass', function () {
-    var path = "./assets/";
-    console.log(path);
-    gulp.watch(path + 'scss/**/*.scss', gulp.series('build-sourcemap-css'));
+gulp.task('copyhtml', async () => {
+    gulp.src('html/html/*.html')
+        .pipe(gulp.dest('dist/html'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('build-sourcemap-css', function () {
-    var path = "./assets/";
-    var mainSassFile = path + "css";
-    return gulp.src(path + 'scss/**/*.scss')
-        .pipe(sourcemaps.init()) // Process the original sources
+gulp.task('imageMin', async () => {
+    gulp.src('html/assets/images/*')
+        .pipe(imagemin())
+            .pipe(gulp.dest('dist/assets/images'));
+});
+
+// gulp.task('minifycss', async () => {
+//     gulp.src('dist/assets/css/*.css')
+//         .pipe(uglify('main-style-min.css'))
+//             .pipe(gulp.dest('dist/assets/css'));
+// });
+
+gulp.task('concat', async () => {
+    gulp.src('html/assets/js/*.js')
+        .pipe(concat('main.js'))
+            .pipe(uglify())
+                .pipe(gulp.dest('dist/assets/js'));
+});
+
+gulp.task('sass', async () => {
+    gulp.src('html/assets/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.write()) // Add the map to modified source.
-        .pipe(gulp.dest(mainSassFile));
+            .pipe(gulp.dest('dist/assets/css'))
+                .pipe(browserSync.stream());
 });
 
 
-gulp.task('minify-css', () => {
-    return gulp.src(`./assets/css/main.css`)
-        .pipe(cleanCSS({
-            compatibility: 'ie8'
-        }))
-        .pipe(concat('style.min.css')) // this is what was missing
-        .pipe(gulp.dest(`./assets/css`));
+gulp.task('build', gulp.series('copyhtml', 'concat', 'imageMin', 'sass'));
+gulp.task('server', function(){
+    browserSync.init({server: './html'});
 });
 
-gulp.task('minify-js', function () {
-    const mapping = {
-        'jsmin': {
-            'path': './assets/js/',
-            'dest': 'global-min.js',
-            'src': [
-                './assets/js/src/global.js',
-                './assets/js/src/slick.min.js',
-                './assets/js/src/aos.min.js',
-                './assets/js/src/fancy-video.js'
-            ]
-        }
-    };
-
-    let data;
-    data = mapping.jsmin;
-
-    return gulp.src(data.src, {
-            allowEmpty: true
-        })
-        .pipe(concat(data.dest))
-        .pipe(gulp.dest(data.path))
+// Watch files for changes
+gulp.task('watch', () => {
+    gulp.watch('html/assets/sass/*.scss').on('change', browserSync.reload);
+    gulp.watch('html/html/*.html').on('change', browserSync.reload);
 });
 
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./html/"
-        }
-    });
-});
-
-
-gulp.task('build', gulp.series('build-sourcemap-css', 'minify-js', 'minify-css'));
+gulp.task('default', gulp.series('server', 'watch'));
